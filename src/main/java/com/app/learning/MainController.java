73aca5d671595817465c8b0da3554e0b4f4d725e
hiteshdhake <hiteshdhake@gmail.com>
2020-12-23 18:23:45 +0530

    Created the simple application in spring boot Which will run on port 8085 as mention in application.properties Created a controller for handling the request and send the response

--- src/main/java/com/app/learning/MainController.java
+++ src/main/java/com/app/learning/MainController.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.app.learning;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class DemoController {
+public class MainController {
 
     @RequestMapping
-    public String hello() {
+    public String echo() {
         return "Hello this is my first Sprint boot application which runs on 8085";
     }
     
